746a25c91a88588662a4fc3745a1b220670dfa7f
KevinTAZARO <kenee23@hotmail.fr>
2022-10-04 18:04:29 +0200

    last commit

---
+++ pyramide.rb
@@ -0,0 +1,14 @@
+print "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+print "> "
+
+psize = gets.chomp.to_i
+
+psize.times do |i|
+(psize - i).times do
+  print " "
+end
+(1+i).times do
+print "*"
+end
+puts
+end
